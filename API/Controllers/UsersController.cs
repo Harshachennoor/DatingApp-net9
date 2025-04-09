4e2a36e0af8f10067a1f5e51623e171222226e4c
Harshachennoor <harshachennoor@gmail.com>
2025-04-08 20:13:38 -0400

    Section 4 Completed

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,15 +1,15 @@
 using System;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")] // https:localhost:port/api/users
-public class UsersController(DataContext context) : ControllerBase
+public class UsersController(DataContext context) : BaseApiController
 {
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
@@ -18,6 +18,7 @@ public class UsersController(DataContext context) : ControllerBase
         return Ok(users);
     }
     
+    [Authorize]
     [HttpGet("{id:int}")] // https:localhost:port/api/users/2
     public async Task<ActionResult<AppUser>> GetUsers(int id)
     {
--- API/Program.cs
+++ API/Program.cs
@@ -1,23 +1,23 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-//COnfigure the HTTP request pipeline
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
+//Configure the HTTP request pipeline
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200","https://localhost:4200"));
+
+//Authenticate {place it above the mapcontrollers}
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
